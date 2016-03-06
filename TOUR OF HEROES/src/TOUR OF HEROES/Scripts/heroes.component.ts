5bfcd9fde7de8160b9e8a4f190b6446a4afa05c6
Truong Nguyen <samuraitruong@hotmail.com>
2016-03-06 16:10:39 +1100

    aa

---
+++ TOUR OF HEROES/src/TOUR OF HEROES/Scripts/heroes.component.ts
@@ -0,0 +1,39 @@
+﻿import {Component} from 'angular2/core';
+import { Router } from 'angular2/router';
+import {Hero} from './models/hero';
+import {HeroDetailComponent} from './hero-detail.component';
+import {OnInit} from 'angular2/core';
+import { HeroService }     from './services/hero.service';
+
+@Component({
+    selector: 'heroes-list',
+    templateUrl: '/app/views/hero-list.html',
+    styleUrls: ['app/content/css/hero-list.css'],
+    directives: [HeroDetailComponent]
+})
+
+export class HeroesComponent implements OnInit {
+    ngOnInit() {
+        this.getHeroes();
+    }
+    constructor(private _heroService: HeroService, private _router: Router) {
+    }
+
+    title: string = 'My AngularJS Application'
+
+    heroes: Hero[]
+    selectedHero: Hero
+    onSelect(h: Hero) {
+        this.selectedHero = h;
+    }
+    gotoDetail() {
+        let link = ['HeroDetail', { id: this.selectedHero.id }];
+        this._router.navigate(link);
+    }
+
+    getHeroes()
+    {
+        this._heroService.asyncGetHeroes().then(list=> this.heroes = list);
+    }
+
+}
---
+++ TOUR OF HEROES/src/TOUR OF HEROES/wwwroot/app/heroes.component.js
@@ -0,0 +1,46 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('angular2/core');
+var router_1 = require('angular2/router');
+var hero_detail_component_1 = require('./hero-detail.component');
+var hero_service_1 = require('./services/hero.service');
+var HeroesComponent = (function () {
+    function HeroesComponent(_heroService, _router) {
+        this._heroService = _heroService;
+        this._router = _router;
+        this.title = 'My AngularJS Application';
+    }
+    HeroesComponent.prototype.ngOnInit = function () {
+        this.getHeroes();
+    };
+    HeroesComponent.prototype.onSelect = function (h) {
+        this.selectedHero = h;
+    };
+    HeroesComponent.prototype.gotoDetail = function () {
+        var link = ['HeroDetail', { id: this.selectedHero.id }];
+        this._router.navigate(link);
+    };
+    HeroesComponent.prototype.getHeroes = function () {
+        var _this = this;
+        this._heroService.asyncGetHeroes().then(function (list) { return _this.heroes = list; });
+    };
+    HeroesComponent = __decorate([
+        core_1.Component({
+            selector: 'heroes-list',
+            templateUrl: '/app/views/hero-list.html',
+            styleUrls: ['app/content/css/hero-list.css'],
+            directives: [hero_detail_component_1.HeroDetailComponent]
+        }), 
+        __metadata('design:paramtypes', [hero_service_1.HeroService, router_1.Router])
+    ], HeroesComponent);
+    return HeroesComponent;
+})();
+exports.HeroesComponent = HeroesComponent;
+//# sourceMappingURL=heroes.component.js.map
